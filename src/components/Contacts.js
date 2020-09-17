80fbd554e0e94652d0e858eea4ec71a03f62f848
Denzil Vorster <50702728+HunterDDV@users.noreply.github.com>
2020-09-17 23:42:51 +0200

    Footer fix
    -Footer position fixed
    -Mobile edges fixed

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -34,43 +34,47 @@ export default () => {
 
     return (
         <div className="container">
-            <div className="Header">
-                <h1 className="heading">Browse the employee list</h1>
-                <h1 className="headingSub">Choose the perfect candidate to employ for your position!</h1>
-            </div>
+            <div className = "holder">
+                <div className="Header">
+                    <h1 className="heading">Browse the employee list</h1>
+                    <h1 className="headingSub">Choose the perfect candidate to employ for your position!</h1>
+                </div>
 
-            <div className="search-box">
-                <input
-                    type="text"
-                    className="search-bar"
-                    placeholder="Search..."
-                    onChange={e => setSearch(e.target.value)}
-                />
-            </div>
+                <div className="search-box">
+                    <input
+                        type="text"
+                        className="search-bar"
+                        placeholder="Search..."
+                        onChange={e => setSearch(e.target.value)}
+                    />
+                </div>
 
-            <div className = "ScrollMenu">
-                <ul className="users">
-                    {
-                        filteredUsers.map(user => <li key={user.name}>
+                <div className="ScrollMenu">
+                    <ul className="users">
+                        {
+                            filteredUsers.map(user => <li key={user.name}>
 
-                            {/* user details */}
-                            <span className="name">{user.name}</span><br />
-                            <span className="username">Username: {user.username}</span><br />
-                            <span>E-mail: </span><span className="email">{user.email}</span><br />
-                            <span className="phone">Phone: {user.phone}</span><br />
-                            <span className="web">Website: {user.website}</span><br />
-                            <span className="addressname">Address:</span> <span className="address">{user.address.street}</span><br />
-                            <span className="code">{user.address.suite}</span><br />
-                            <span className="code">{user.address.zipcode}</span><br />
-                            <span className="companyname">Company:</span> <span className="company">{user.company.name}</span><br />
-                            <span className="code">{user.company.catchPhrase}</span><br />
-                            <span className="code">{user.company.bs}</span><br />
-                        </li>)
-                    }
-                </ul>
-            </div>
-            <div className = "footer">
+                                {/* user details */}
+                                <span className="name">{user.name}</span><br />
+                                <span className="username">Username: {user.username}</span><br />
+                                <span>E-mail: </span><span className="email">{user.email}</span><br />
+                                <span className="phone">Phone: {user.phone}</span><br />
+                                <span className="web">Website: {user.website}</span><br />
+                                <span className="addressname">Address:</span> <span className="address">{user.address.street}</span><br />
+                                <span className="code">{user.address.suite}</span><br />
+                                <span className="code">{user.address.zipcode}</span><br />
+                                <span className="companyname">Company:</span> <span className="company">{user.company.name}</span><br />
+                                <span className="code">{user.company.catchPhrase}</span><br />
+                                <span className="code">{user.company.bs}</span><br />
+                            </li>)
+                        }
+                    </ul>
+                </div>
+
+
+                <div className="footer">
                     <p>Making sure you make the right choice!</p>
+                </div>
             </div>
         </div>
     )
